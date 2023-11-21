916310f8965460cb75f46f2aadc152c15adfe999
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-11-21 20:01:12 -0300

    Atividade 13 JavaScript

---
+++ Curso_em_Video-JavaScript/exercicios/ex013/estrutura_de_repeticao.js
@@ -0,0 +1,40 @@
+var bombom = 16
+while(bombom > 0){
+    -- bombom/
+    console.log('- 1 Bom-Bom, restam (' + bombom + ')unidades')
+    
+    if(bombom == 0){
+        console.log('acabou o bombom :(')
+    }
+}
+
+/* 
+var c = 1
+while (c<=10){
+    c++
+} 
+*/
+
+//-----WHILE-----//
+var a = 1 
+while(a<=10){
+    console.log(a)
+    ++a
+}
+
+//-----DO WHILE-----//
+var b = 1 
+do{
+    console.log(b)
+    ++b
+}while(b<=10)
+
+//-----FOR-----//
+for(var c=1; c<= 10; c++){
+    console.log(c)
+}
+/*  var c = 1
+    while (c<=10){
+        c++
+    } */
+//----- -----//
