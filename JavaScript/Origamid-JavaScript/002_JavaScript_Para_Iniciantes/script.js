0429b4c13c4a3724d85f815338054146c9f97b1c
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-12-09 13:15:16 -0300

    Iniciando JavaScript - Origamid

---
+++ JavaScript/Origamid-JavaScript/002_JavaScript_Para_Iniciantes/script.js
@@ -0,0 +1,9 @@
+var num = 69
+console.log(`${num} = ${typeof num}`) 
+
+var nome = "Otávio"
+console.log(`${nome} = ${typeof nome}`) 
+
+var simbolo = symbol()
+console.log(`${simbolo} = ${typeof simbolo}`) 
+
