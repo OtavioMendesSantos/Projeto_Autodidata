1c7563b85ed1259e6abf8731f7fb8c3c7e57ab85
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-12-09 15:02:25 -0300

    Exercício Numeros e Operadores

---
+++ JavaScript/Origamid-JavaScript/002_JavaScript_Para_Iniciantes/Numeros_E_Operadores/script.js
@@ -0,0 +1,24 @@
+// Qual o resultado da seguinte expressão? = 35
+var total = 10 + 5 * 2 / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+let texto = "olá"
+console.log(+texto)
+console.log(-texto)
+
+// Somar a string '200' com o número 50 e retornar 250
+let x = 50
+let y = "200"
+console.log(+ y + x)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+let c = 0
+c += 5
+console.log(c)
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;  
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(peso)
+
