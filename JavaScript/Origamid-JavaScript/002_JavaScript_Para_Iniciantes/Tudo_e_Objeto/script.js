8ab28925542c46a2471c3415d5b7c907d58a05e5
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-12-27 17:59:25 -0300

    Exercícios Tudo é Objeto

---
+++ JavaScript/Origamid-JavaScript/002_JavaScript_Para_Iniciantes/Tudo_e_Objeto/script.js
@@ -0,0 +1,18 @@
+// nomeie 3 propriedades ou métodos de strings
+let nome = "Otávio Mendes Santos"
+console.log(nome.length)
+console.log(nome.toUpperCase())
+console.log(nome.charAt(2))
+console.log(nome.search('áv'))
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+/* 
+.getElementById
+.getElementByClassName
+.querySelector
+.innerHTML
+.style
+*/
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+// https://clipboardjs.com
