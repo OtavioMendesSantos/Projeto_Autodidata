7e88658e378e707fc2af6644a5ddccb2f842f53b
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-12-28 01:47:23 -0300

    Exercício Escopo

---
+++ JavaScript/Origamid-JavaScript/002_JavaScript_Para_Iniciantes/escopo/script.js
@@ -0,0 +1,29 @@
+// Por qual motivo o código abaixo retorna com erros? - pois var é um tipo de variável e não possui valor
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+}
+//console.log(var, marca, portas);
+  
+  // Como corrigir o erro abaixo? -- Basta tirar a variável dois de dentro da function.
+  const dois = 2;
+  function somarDois(x) {
+    /* const dois = 2; */
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x + dois;
+  }
+  console.log(somarDois(4));
+  console.log(dividirDois(6));
+  
+// O que fazer para total retornar 500?
+  var numero = 50;
+  
+  for(let numero = 0; numero < 10; numero++) {
+    console.log(numero);
+  }
+  
+  const total = 10 * numero;
+  console.log(total);
