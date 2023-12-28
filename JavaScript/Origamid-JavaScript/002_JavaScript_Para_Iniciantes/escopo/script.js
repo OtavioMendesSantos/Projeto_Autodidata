062c4560edfe136b36982f75fd354d181b5290c7
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-12-28 01:53:45 -0300

    Exercício Escopo

--- JavaScript/Origamid-JavaScript/002_JavaScript_Para_Iniciantes/escopo/script.js
+++ JavaScript/Origamid-JavaScript/002_JavaScript_Para_Iniciantes/escopo/script.js
@@ -3,23 +3,24 @@
     var cor = 'preto';
     const marca = 'Fiat';
     let portas = 4;
+    console.log(cor, marca, portas)
 }
 //console.log(var, marca, portas);
-  
-  // Como corrigir o erro abaixo? -- Basta tirar a variável dois de dentro da function.
-  const dois = 2;
-  function somarDois(x) {
-    /* const dois = 2; */
-    return x + dois;
-  }
-  function dividirDois(x) {
-    return x + dois;
-  }
-  console.log(somarDois(4));
-  console.log(dividirDois(6));
+
+// Como corrigir o erro abaixo? -- Basta tirar a variável dois de dentro da function.
+const dois = 2;
+function somarDois(x) {
+  /* const dois = 2; */
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+      console.log(somarDois(4));
+      console.log(dividirDois(6));  
   
 // O que fazer para total retornar 500?
-  var numero = 50;
+  const numero = 50;
   
   for(let numero = 0; numero < 10; numero++) {
     console.log(numero);
