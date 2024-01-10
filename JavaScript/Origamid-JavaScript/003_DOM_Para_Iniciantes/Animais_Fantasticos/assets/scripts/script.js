41e0b47ef04bf643ada047fba1473275e5284484
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-01-10 11:44:55 -0300

    Exercício Animação ao Scroll

--- JavaScript/Origamid-JavaScript/003_DOM_Para_Iniciantes/Animais_Fantasticos/assets/scripts/script.js
+++ JavaScript/Origamid-JavaScript/003_DOM_Para_Iniciantes/Animais_Fantasticos/assets/scripts/script.js
@@ -69,3 +69,27 @@ function initScrollSuave(){
   })
 }
 initScrollSuave()
+
+//Exercício Animação ao Scroll
+function initAnimaScroll(){
+  const sections = document.querySelectorAll('.js-scroll')
+  if(sections){
+    const windowMetade = window.innerHeight * 0.75
+
+    function animaScroll(){
+      sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top
+        const isSectionVisible = (sectionTop - windowMetade) < 0
+        if(isSectionVisible){
+          section.classList.add('ativo')
+        } else {
+          section.classList.remove('ativo')
+        }
+      })
+    }
+  }
+  animaScroll()
+
+  window.addEventListener('scroll', animaScroll)
+}
+initAnimaScroll()
