f8990cc77f2cb20d385397a8c0d0ebcb426d0b0e
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-01-10 10:31:14 -0300

    Exercício Scroll Suave Link Interno

--- JavaScript/Origamid-JavaScript/003_DOM_Para_Iniciantes/Animais_Fantasticos/assets/scripts/script.js
+++ JavaScript/Origamid-JavaScript/003_DOM_Para_Iniciantes/Animais_Fantasticos/assets/scripts/script.js
@@ -41,3 +41,31 @@ function initAccordion(){
   }
 }
 initAccordion()
+
+//Exercício Scroll Suave Link Interno
+function initScrollSuave(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+  function scrollToSecction(event){
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({ /* PENDENTE: Ver isso */
+      behavior:'smooth',
+      block: 'start'
+    })
+    
+    //forma alternativa
+    /* 
+    sectionTop = section.offsetTop
+    window.scrollTo({
+      top: sectionTop,
+      behavior: "smooth"
+    })
+    */
+  }
+
+  linksInternos.forEach((link)=>{
+    link.addEventListener('click', scrollToSecction)
+  })
+}
+initScrollSuave()
