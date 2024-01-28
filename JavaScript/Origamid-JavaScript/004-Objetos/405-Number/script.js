d6b5cc219aafcc1827d7449e6801faee85fe0190
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-01-28 12:28:04 -0300

    Exercício Number e Math

--- JavaScript/Origamid-JavaScript/004-Objetos/405-Number/script.js
+++ JavaScript/Origamid-JavaScript/004-Objetos/405-Number/script.js
@@ -3,17 +3,28 @@
 function numeroAleatorio(min,max){
   return Math.round((Math.random() * (max - min)) + min)
 }
-for(i = 0; i < 11; i++){
-  console.log(numeroAleatorio(1050, 2000))
-  console.log(numeroAleatorio(1, 2))
-}
+console.log(numeroAleatorio(1050, 2000))
 
 // Retorne o maior número da lista abaixo
 const numeros = '4, 5, 20, 8, 9';
-console.log(Math.max(test))
+arrayNumeros = numeros.split(', ')
+console.log(Math.max(...arrayNumeros))
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-                     'R$ 230  ', 'r$  200'];
+const listaPrecos = ['R$ 59,99', ' R$ 100,222', 'R$ 230  ', 'r$  200'];
+
+function limpaPreco(preco){
+  preco = +preco.toUpperCase().replace('R$','').trim().replace(',','.')
+  preco = +preco.toFixed(2)
+  return preco
+}
+
+let soma = 0;
+listaPrecos.forEach((item)=>{
+ soma += limpaPreco(item)
+})
+
+
+console.log(soma.toLocaleString('pt-BR',{style:'currency', currency:'BRL'}))
