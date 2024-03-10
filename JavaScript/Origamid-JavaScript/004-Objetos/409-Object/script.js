7b726cb9afc1e05cd9f641e433a003f21dca27a5
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-03-10 15:02:09 -0300

    Exercício Object

--- JavaScript/Origamid-JavaScript/004-Objetos/409-Object/script.js
+++ JavaScript/Origamid-JavaScript/004-Objetos/409-Object/script.js
@@ -1,21 +1,38 @@
-const funcaoAutomovel = {
-  acelerar() {
-    return 'acelerou';
-  },
-  buzinar() {
-    return 'buzinou';
-  },
+// Crie uma função que verifique corretamente o tipo de dado
+
+function verificaDado(dado){
+  return Object.prototype.toString.call(dado)
 }
 
-const moto = {
-  rodas: 2,
-  capacete: true,
+console.log(verificaDado([]))
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+
+const quadrado = {}
+
+Object.defineProperties(quadrado,{
+  lados:{
+    value: 4,
+    enumerable: true,
+  }
+})
+
+// Previna qualquer mudança
+// no objeto abaixo
+
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
 }
 
-const carro = {
-  rodas: 4,
-  mala: true,
-}
+Object.freeze(configuracao)
 
-Object.assign(moto, funcaoAutomovel);
-Object.assign(carro, funcaoAutomovel); 
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+
+console.log(Object.getOwnPropertyNames(String.prototype))
+console.log(Object.getOwnPropertyNames(Array.prototype))
