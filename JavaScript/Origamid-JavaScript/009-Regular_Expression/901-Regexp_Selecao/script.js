8bbdd6b36f175e78c85e7e5c7b6eb0ae07f5df81
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-04-14 04:02:47 -0300

    :black_nib:	course: Reg Exp Seleção

---
+++ JavaScript/Origamid-JavaScript/009-Regular_Expression/901-Regexp_Selecao/script.js
@@ -0,0 +1,10 @@
+// Procura: a
+const padraoRegexp = /a/;
+
+const texto = 'JavaScript';
+const novoTexto = texto.replace(padraoRegexp, 'B');
+// JBvaScript
+
+console.log(novoTexto);
+
+/* Site para testar Regexp: https://regexr.com/7usvt */
