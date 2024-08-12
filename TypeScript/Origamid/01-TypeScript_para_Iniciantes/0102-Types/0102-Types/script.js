9d29da1711c983df92fe89d716f85c6817d94b7e
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-08-11 21:46:08 -0300

    :black_nib:	course: 102 Types

---
+++ TypeScript/Origamid/01-TypeScript_para_Iniciantes/0102-Types/0102-Types/script.js
@@ -0,0 +1,16 @@
+//@ts-check
+
+const frase = 'Hello, World!';
+const total = 100.05
+const empresas = ['Microsoft', 'Google', 'Apple']
+const body = document.body
+const button = document.querySelector('button')
+
+frase.toLowerCase()
+// total.toLowerCase()
+
+total.toFixed()
+empresas[0].toLowerCase()
+body.style.backgroundColor = 'red'
+button?.click() // erro
+console.log(frase.toLowerCase())
