051553e8bb555c6e67df69426090bed8e82e8bb3
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-08-13 10:28:31 -0300

    :black_nib:	course: 0202 String, Number e Boolean

---
+++ TypeScript/Origamid/02-TypeScript_Basico/0202-String_Number_e_Boolean/script.js
@@ -0,0 +1,18 @@
+"use strict";
+// const frase: string = "Front End";
+// const preco: number = 500;
+// const condi: boolean = preco > 100;
+// console.log(typeof frase);
+// console.log(typeof preco);
+// console.log(typeof condi);
+// if (typeof frase === "string") {
+//   console.log("Frase é uma string");
+// } else {
+//   console.log("Não é uma string");
+// }
+const frase1 = new String('Front End');
+const frase2 = String('Front End');
+const frase3 = 'Front End';
+console.log(typeof frase1);
+console.log(typeof frase2);
+console.log(typeof frase3);
