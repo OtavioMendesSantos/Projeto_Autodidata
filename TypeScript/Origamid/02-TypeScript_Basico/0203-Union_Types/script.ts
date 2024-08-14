bcb87e57dfb788a1de4db72c58a659ec353b7528
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-08-14 20:37:37 -0300

    :black_nib:	course: 0203 Union Types 2

--- TypeScript/Origamid/02-TypeScript_Basico/0203-Union_Types/script.ts
+++ TypeScript/Origamid/02-TypeScript_Basico/0203-Union_Types/script.ts
@@ -1,16 +1,34 @@
-let total: string | number = 200;
-total = "200";
+// let total: string | number = 200;
+// total = "200";
 
-function isNumber(value: string | number): true | false {
-  if (typeof value === "number") {
-    return true;
-  } else {
-    return false;
-  }
+// function isNumber(value: string | number): true | false {
+//   if (typeof value === "number") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isNumber(total));
+// console.log(isNumber(200));
+
+// const button: HTMLButtonElement | null = document.querySelector("button");
+// button?.addEventListener("click", () => console.log("Clicou"));
+
+// * Exercícios
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+
+function toNumber(value:string|number):number {
+    if (typeof value === "number" || typeof value === "string") {
+        return Number(value)
+    } 
+    throw "value deve ser um número ou uma string"
 }
 
-console.log(isNumber(total));
-console.log(isNumber(200));
-
-const button: HTMLButtonElement | null = document.querySelector("button");
-button?.addEventListener("click", () => console.log("Clicou"));
+console.log(toNumber(200))
+console.log(toNumber("200"))
+console.log(toNumber({}))
