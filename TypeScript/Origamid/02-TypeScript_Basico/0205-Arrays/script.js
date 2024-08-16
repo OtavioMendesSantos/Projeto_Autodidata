50d42f19b8ebc27b85b1fd8040484b3786fe9bc1
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-08-16 10:41:40 -0300

    :black_nib:	course: 0205 Arrays 1

---
+++ TypeScript/Origamid/02-TypeScript_Basico/0205-Arrays/script.js
@@ -0,0 +1,17 @@
+"use strict";
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const valores = ["1", 2, 3, 4, "5", 6, 7, 8, 9, 10, "11"];
+function maiorQue10(data) {
+    return data.filter((item) => item > 10);
+}
+// (number | string)[]
+function filtrarValores(data) {
+    return data.filter((item) => typeof item === "number");
+}
+console.log(maiorQue10(numeros));
+console.log(filtrarValores(valores));
+const dados = [
+    ['Harry Potter 0', 15],
+    ['Harry Potter 1', 20],
+    ['Harry Potter 2', 30],
+];
