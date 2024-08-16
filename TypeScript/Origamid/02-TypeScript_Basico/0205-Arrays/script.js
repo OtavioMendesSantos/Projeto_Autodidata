0a4c996a6cd773f476d3da3494ae8f29ad2a7aeb
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-08-16 12:17:41 -0300

    :black_nib:	course: 0205 Arrays 2

--- TypeScript/Origamid/02-TypeScript_Basico/0205-Arrays/script.js
+++ TypeScript/Origamid/02-TypeScript_Basico/0205-Arrays/script.js
@@ -1,17 +1,17 @@
 "use strict";
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const valores = ["1", 2, 3, 4, "5", 6, 7, 8, 9, 10, "11"];
-function maiorQue10(data) {
-    return data.filter((item) => item > 10);
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const valores = ["1", 2, 3, 4, "5", 6, 7, 8, 9, 10, "11"];
+async function fetchCursos() {
+    const response = await fetch("https://api.origamid.dev/json/cursos.json");
+    const data = await response.json();
+    mostrarCursos(data);
 }
-// (number | string)[]
-function filtrarValores(data) {
-    return data.filter((item) => typeof item === "number");
+fetchCursos();
+function mostrarCursos(cursos) {
+    cursos.forEach((item) => {
+        const h1 = document.createElement("h1");
+        h1.style.color = item.nivel === "iniciante" ? "blue" : "red";
+        h1.innerText = item.nome;
+        document.body.appendChild(h1);
+    });
 }
-console.log(maiorQue10(numeros));
-console.log(filtrarValores(valores));
-const dados = [
-    ['Harry Potter 0', 15],
-    ['Harry Potter 1', 20],
-    ['Harry Potter 2', 30],
-];
--- TypeScript/Origamid/02-TypeScript_Basico/0205-Arrays/script.ts
+++ TypeScript/Origamid/02-TypeScript_Basico/0205-Arrays/script.ts
@@ -1,21 +1,56 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const valores = ["1", 2, 3, 4, "5", 6, 7, 8, 9, 10, "11"];
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const valores = ["1", 2, 3, 4, "5", 6, 7, 8, 9, 10, "11"];
 
-function maiorQue10(data: Array<number>): number[] {
-  return data.filter((item) => item > 10);
+// function maiorQue10(data: Array<number>): number[] {
+//   return data.filter((item) => item > 10);
+// }
+
+// // (number | string)[]
+// function filtrarValores(data: Array<number | string>) {
+//   return data.filter((item) => typeof item === "number");
+// }
+
+// console.log(maiorQue10(numeros));
+// console.log(filtrarValores(valores));
+
+// const dados = [
+//   ['Harry Potter 0', 15],
+//   ['Harry Potter 1', 20],
+//   ['Harry Potter 2', 30],
+// ]
+
+/* 
+Defina a interface da API: https://api.origamid.dev/json/cursos.json e mostre os dados na tela.
+Existem apenas dois níveis de cursos, Iniciante (iniciante) e Avançado (avancado). Se for para iniciante pinte o título de azul, para avançado pinte de vermelho. 
+*/
+
+type Nivel = "iniciante" | "avancado";
+
+interface Curso {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: boolean;
+  tags: Array<string>;
+  idAulas: Array<number>;
+  nivel: Nivel;
 }
 
-// (number | string)[]
-function filtrarValores(data: Array<number | string>) {
-  return data.filter((item) => typeof item === "number");
+type Cursos = Array<Curso>;
+
+async function fetchCursos() {
+  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+  const data = await response.json();
+  mostrarCursos(data);
 }
 
-console.log(maiorQue10(numeros));
-console.log(filtrarValores(valores));
+fetchCursos();
 
-
-const dados = [
-  ['Harry Potter 0', 15],
-  ['Harry Potter 1', 20],
-  ['Harry Potter 2', 30],
-]
+function mostrarCursos(cursos: Cursos) {
+  cursos.forEach((item) => {
+    const h1 = document.createElement("h1");
+    h1.style.color = item.nivel === "iniciante" ? "blue" : "red";
+    h1.innerText = item.nome;
+    document.body.appendChild(h1);
+  })
+}
