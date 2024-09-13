628d475160b23cbd6e7ee7278ad8a757e55f381a
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-09-13 11:11:44 -0300

    :black_nib: course: 0302 Interfaces do DOM 2

--- TypeScript/Origamid/03-Objetos_e_Interfaces/0302-Interfaces_do_DOM/script.ts
+++ TypeScript/Origamid/03-Objetos_e_Interfaces/0302-Interfaces_do_DOM/script.ts
@@ -1,9 +1,25 @@
-const video = document.querySelector('video');
-const anchor = document.querySelector('a');
-const img = document.querySelector('img');
+// const video = document.querySelector('video');
+// const anchor = document.querySelector('a');
+// const img = document.querySelector('img');
 
-if (video instanceof HTMLVideoElement) {
-  console.log(video.currentTime);
-  console.log(video.volume);
-  
-}
+// if (video instanceof HTMLVideoElement) {
+//   console.log(video.currentTime);
+//   console.log(video.volume);
+// }
+
+const links = document.querySelectorAll('.link');
+
+// console.log(NodeListOf) -> Interface != classe  
+// console.log(NodeList) -> Classe 
+
+links.forEach((link) => {
+  if (link instanceof HTMLAnchorElement){
+    console.log(link.href)
+  } else {
+    console.log(typeof link)
+  }
+})
+
+// links.filter - Não funciona
+const ArrayDeLinks = Array.from(links)
+console.log(ArrayDeLinks.filter((link) => link instanceof HTMLAnchorElement))
