d370c90b3dc3c0e4fac379025c64c5b2f5e437f8
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-09-13 11:23:32 -0300

    :black_nib:	course: 0302 Interfaces do DOM 3

--- TypeScript/Origamid/03-Objetos_e_Interfaces/0302-Interfaces_do_DOM/script.ts
+++ TypeScript/Origamid/03-Objetos_e_Interfaces/0302-Interfaces_do_DOM/script.ts
@@ -7,19 +7,37 @@
 //   console.log(video.volume);
 // }
 
-const links = document.querySelectorAll('.link');
+// const links = document.querySelectorAll('.link');
 
-// console.log(NodeListOf) -> Interface != classe  
-// console.log(NodeList) -> Classe 
+// // console.log(NodeListOf) -> Interface != classe
+// // console.log(NodeList) -> Classe
+
+// links.forEach((link) => {
+//   if (link instanceof HTMLAnchorElement){
+//     console.log(link.href)
+//   } else {
+//     console.log(typeof link)
+//   }
+// })
+
+// // links.filter - Não funciona
+// const ArrayDeLinks = Array.from(links)
+// console.log(ArrayDeLinks.filter((link) => link instanceof HTMLAnchorElement))
+
+// 1 - Selecione os elementos com a classe link.
+
+// 2 - Crie uma função que deve ser executada para cada elemento.
+
+// 3 - Modificar através da função o estilo da color e border.
+
+const links = document.querySelectorAll(".link");
+
+function ativarElemento(el: HTMLElement) {
+  el.style.color = "red";
+  el.style.border = "1px solid red";
+}
 
 links.forEach((link) => {
-  if (link instanceof HTMLAnchorElement){
-    console.log(link.href)
-  } else {
-    console.log(typeof link)
-  }
-})
-
-// links.filter - Não funciona
-const ArrayDeLinks = Array.from(links)
-console.log(ArrayDeLinks.filter((link) => link instanceof HTMLAnchorElement))
+  console.log(link.__proto__)
+  if (link instanceof HTMLElement) { ativarElemento(link) } }
+);
