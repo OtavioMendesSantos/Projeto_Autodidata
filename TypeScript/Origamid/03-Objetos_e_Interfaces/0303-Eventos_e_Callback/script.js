043665700ce89bae1865caeb1985067c7f1373dc
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-09-27 14:03:52 -0300

    :black_nib:	course: 0303 Eventos e Callback 3

--- TypeScript/Origamid/03-Objetos_e_Interfaces/0303-Eventos_e_Callback/script.js
+++ TypeScript/Origamid/03-Objetos_e_Interfaces/0303-Eventos_e_Callback/script.js
@@ -1,13 +1,17 @@
 "use strict";
-// const btn = document.querySelector("button");
+/* // const btn = document.querySelector("button");
 // function handleClick(e: PointerEvent) {
 //     console.log(e);
 // }
+
 // btn?.addEventListener("pointerdown", handleClick);
+
 // function handleScroll (event: Event) {
 //     console.log(event);
 // }
+
 // window.addEventListener("scroll", handleScroll);
+
 // function handleAtivarMenu(event: Event) {
 //     if (event instanceof MouseEvent) {
 //         console.log(event.pageX, event.pageY);
@@ -16,19 +20,44 @@
 //         console.log(event.touches);
 //     }
 // }
+
 // document.documentElement.addEventListener("click", handleAtivarMenu);
 // document.documentElement.addEventListener("touchstart", handleAtivarMenu);
 // document.documentElement.addEventListener("keydown", handleAtivarMenu);
+
 // const btn = document.querySelector("button");
 // function handleClick(this: HTMLButtonElement, e: MouseEvent){
 //     console.log(this);
 // }
 // btn?.addEventListener("click", handleClick);
-const btn = document.querySelector("button");
-function handleClick(e) {
-    const el = e.currentTarget;
-    if (el instanceof HTMLElement) {
-        console.log(el.innerText);
+
+// const btn = document.querySelector("button");
+// function handleClick(e: MouseEvent) {
+//   const el = e.currentTarget;
+//   if (el instanceof HTMLElement) {
+//     console.log(el.innerText);
+//   }
+// }
+// btn?.addEventListener("click", handleClick); */
+// Utilizando a estrutura HTML/CSS abaixo,
+// crie o script que irá fazer o botão mobile funcionar (ativar/desativar a navegação).
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+const btn = document.getElementById("btn-mobile");
+const toogleMenu = (event) => {
+    const nav = document.getElementById("nav");
+    const button = event.currentTarget;
+    if (button instanceof HTMLElement && nav) {
+        nav.classList.toggle("active");
+        button.setAttribute("aria-expanded", nav.classList.contains("active") ? "true" : "false");
+        button.setAttribute("aria-label", nav.classList.contains("active") ? "Fechar Menu" : "Abrir Menu");
     }
-}
-btn?.addEventListener("click", handleClick);
+};
+btn?.addEventListener("pointerdown", toogleMenu);
--- TypeScript/Origamid/03-Objetos_e_Interfaces/0303-Eventos_e_Callback/script.ts
+++ TypeScript/Origamid/03-Objetos_e_Interfaces/0303-Eventos_e_Callback/script.ts
@@ -1,4 +1,4 @@
-// const btn = document.querySelector("button");
+/* // const btn = document.querySelector("button");
 // function handleClick(e: PointerEvent) {
 //     console.log(e);
 // }
@@ -30,11 +30,40 @@
 // }
 // btn?.addEventListener("click", handleClick);
 
-const btn = document.querySelector("button");
-function handleClick(e: MouseEvent) {
-  const el = e.currentTarget;
-  if (el instanceof HTMLElement) {
-    console.log(el.innerText);
+// const btn = document.querySelector("button");
+// function handleClick(e: MouseEvent) {
+//   const el = e.currentTarget;
+//   if (el instanceof HTMLElement) {
+//     console.log(el.innerText);
+//   }
+// }
+// btn?.addEventListener("click", handleClick); */
+
+// Utilizando a estrutura HTML/CSS abaixo,
+// crie o script que irá fazer o botão mobile funcionar (ativar/desativar a navegação).
+
+// Estado dos elementos
+
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+
+const btn = document.getElementById("btn-mobile");
+const toogleMenu = (event: PointerEvent) => {
+  const nav = document.getElementById("nav");
+  const button = event.currentTarget
+  
+  if (button instanceof HTMLElement && nav) {
+    nav.classList.toggle("active");
+    button.setAttribute("aria-expanded", nav.classList.contains("active") ? "true" : "false");
+    button.setAttribute("aria-label", nav.classList.contains("active") ? "Fechar Menu" : "Abrir Menu");
   }
-}
-btn?.addEventListener("click", handleClick);
+};
+
+btn?.addEventListener("pointerdown", toogleMenu);
