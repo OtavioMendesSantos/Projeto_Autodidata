d10b66f25606a0a4f52846925732a34d991bc0b3
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2025-01-05 15:30:50 -0300

    :black_nib:	course: 0308 User Type Guard 2

--- TypeScript/Origamid/03-Objetos_e_Interfaces/0308-User_Type_Guard/script.js
+++ TypeScript/Origamid/03-Objetos_e_Interfaces/0308-User_Type_Guard/script.js
@@ -21,3 +21,25 @@ const handleData = (data) => {
         data.toUpperCase();
     }
 };
+async function fetchProduto() {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const json = await response.json();
+    handleProduto(json);
+}
+fetchProduto();
+function isProduto(value) {
+    if (value &&
+        typeof value === "object" &&
+        "nome" in value &&
+        "preco" in value) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function handleProduto(data) {
+    if (isProduto(data)) {
+        console.log(data);
+    }
+}
--- TypeScript/Origamid/03-Objetos_e_Interfaces/0308-User_Type_Guard/script.ts
+++ TypeScript/Origamid/03-Objetos_e_Interfaces/0308-User_Type_Guard/script.ts
@@ -22,4 +22,35 @@ const handleData = (data: unknown) => {
   if (isString(data)) {
     data.toUpperCase();
   }
+};
+
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
+fetchProduto();
+
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+  if (
+    value &&
+    typeof value === "object" &&
+    "nome" in value &&
+    "preco" in value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleProduto(data: unknown) {
+  if (isProduto(data)) {
+    console.log(data);
+  }
 }
