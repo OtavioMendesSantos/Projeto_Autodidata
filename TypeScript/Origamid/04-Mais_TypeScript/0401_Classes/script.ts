c178a6f0016fca2cc461253f9ea737c135a0365a
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2025-01-06 23:51:08 -0300

    :black_nib: course: 0401 Classes

---
+++ TypeScript/Origamid/04-Mais_TypeScript/0401_Classes/script.ts
@@ -0,0 +1,86 @@
+console.log(document.constructor);
+
+class Produto {
+  readonly tipo = "Livro";
+  private ISBN = "123";
+  protected editora = "Editora A";
+  nome: string;
+  valor: number;
+
+  constructor(nome: string, valor: number) {
+    this.nome = nome;
+    this.valor = valor;
+  }
+
+  getPreco() {
+    return Produto.transformarPreco(this.valor);
+  }
+
+  static transformarPreco(preco : number) {
+    return `R$ ${preco }`
+  }
+}
+
+const livro = new Produto("Dorohedoro", 10);
+// livro.tipo = 'Livro'
+//  livro.ISBN
+Produto.transformarPreco(10)
+
+class Livro extends Produto {
+  paginas: number;
+  constructor(nome: string, valor: number, paginas: number) {
+    super(nome, valor);
+    this.paginas = paginas;
+  }
+}
+
+class Quadrado {
+  readonly lados = 4;
+  medida: number;
+  constructor(medida: number) {
+    this.medida = medida;
+  }
+  getPerimetro() {
+    return this.medida * this.lados;
+  }
+  getArea() {
+    return this.medida * this.medida;
+  }
+}
+
+class Circulo {
+  readonly PI = Math.PI;
+  raio: number;
+  constructor(raio: number) {
+    this.raio = raio;
+  }
+  getPerimetro() {
+    return Math.round(2 * this.PI * this.raio * 100) / 100;
+  }
+  getArea() {
+    return Math.round(this.PI * (this.raio * this.raio) * 100) / 100;
+  }
+}
+
+const formas = [2, 32, 12, 3, 4, 20, 37, 9].map((n) => {
+  if (n < 15) {
+    return new Quadrado(n);
+  } else {
+    return new Circulo(n);
+  }
+});
+
+formas.forEach((forma) => {
+  if (forma instanceof Quadrado) {
+    console.log(forma.getArea());
+  }
+  if (forma instanceof Circulo) {
+    console.log(forma.getPerimetro());
+  }
+});
+
+const q1 = new Quadrado(10);
+
+if ( q1 instanceof Quadrado) {
+  console.log(q1.getArea())
+}
