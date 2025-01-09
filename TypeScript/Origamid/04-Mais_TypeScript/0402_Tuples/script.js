6cc589db4f800a4717c007fd0a192f195517164d
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2025-01-08 23:55:31 -0300

    :black_nib: course: 0402 Tuples 2

--- TypeScript/Origamid/04-Mais_TypeScript/0402_Tuples/script.js
+++ TypeScript/Origamid/04-Mais_TypeScript/0402_Tuples/script.js
@@ -1,19 +1,38 @@
 "use strict";
-const produto1 = ["Notebook", 2500];
-const produto2 = ["Notebook", 2500];
+/* const produto1: (string | number)[] = ["Notebook", 2500];
+const produto2: [string, number] = ["Notebook", 2500];
+
 const nome1 = produto1[0]; // string | number
 const nome2 = produto2[0]; // string
+
 const [nomeP1, precoP1] = produto1;
 const [nome, preco] = produto2;
-function getText(selector) {
-    const el = document.querySelector(selector);
-    if (el) {
-        return [el, el.innerText];
-    }
-    return null;
+
+function getText(selector: string) {
+  const el = document.querySelector<HTMLElement>(selector);
+  if (el) {
+    return [el, el.innerText] as const;
+  }
+  return null;
 }
+
 const button = getText("button");
+
 if (button) {
-    button[0].innerText = "Comprar";
-    button[1] = "Comprar";
+  button[0].innerText = "Comprar";
+  button[1] = "Comprar";
+}
+ */
+async function fetchVendas() {
+    const response = await fetch("https://api.origamid.dev/json/vendas.json");
+    const data = await response.json();
+    console.log(data);
+    somarVendas(data);
+}
+fetchVendas();
+function somarVendas(data) {
+    const total = data.reduce((acc, venda) => {
+        return acc + venda[1];
+    }, 0);
+    console.log(total);
 }
--- TypeScript/Origamid/04-Mais_TypeScript/0402_Tuples/script.ts
+++ TypeScript/Origamid/04-Mais_TypeScript/0402_Tuples/script.ts
@@ -1,4 +1,4 @@
-const produto1: (string | number)[] = ["Notebook", 2500];
+/* const produto1: (string | number)[] = ["Notebook", 2500];
 const produto2: [string, number] = ["Notebook", 2500];
 
 const nome1 = produto1[0]; // string | number
@@ -21,3 +21,29 @@ if (button) {
   button[0].innerText = "Comprar";
   button[1] = "Comprar";
 }
+ */
+
+async function fetchVendas (){
+  const response = await fetch("https://api.origamid.dev/json/vendas.json");
+  const data = await response.json();
+  console.log(data)
+  somarVendas(data)
+}
+fetchVendas()
+
+interface ProdutoInterface{
+  marca: string;
+  cor: string;
+}
+
+type Venda = [string, number, string, ProdutoInterface];
+
+function somarVendas(data: Venda[]) {
+  
+  const total = data.reduce((acc, venda) => {
+    return acc + venda[1]
+  }, 0)
+
+  console.log(total)
+}
+
