d146bf065f6fa888eceb7e9acbaf5ff3504387a0
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-01-13 23:46:32 -0300

    ✒ course: 0504 Bibliotecas Externas

---
+++ TypeScript/Origamid/05-Desenvolvimento/504-Bibliotecas_Externas/dist/script.d.ts
@@ -0,0 +1,2 @@
+declare const Vimeo: any;
+declare const vimeo: any;
---
+++ TypeScript/Origamid/05-Desenvolvimento/504-Bibliotecas_Externas/dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+$("body").addClass("loaded");
+console.log(_.difference([1, 2, 3], [1, 2, 4]));
+const vimeo = new Vimeo.Player('player');
+vimeo.play();
+//# sourceMappingURL=script.js.map
