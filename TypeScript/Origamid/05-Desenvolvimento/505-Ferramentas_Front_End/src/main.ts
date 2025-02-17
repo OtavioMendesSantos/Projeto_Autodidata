ea9de60742a5b1b502d4b664530c28037666f256
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-02-17 00:06:29 -0300

    :black_nib: course: fix 0505 Ferramentas Front

--- TypeScript/Origamid/05-Desenvolvimento/505-Ferramentas_Front_End/src/main.ts
+++ TypeScript/Origamid/05-Desenvolvimento/505-Ferramentas_Front_End/src/main.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import Clipboard from "clipboard";
+import ClipboardJS from "clipboard";
 
 interface Produto {
   nome: string;
@@ -11,10 +11,11 @@ interface Produto {
 
 console.log(_.difference([1, 2, 3], [1, 2, 4]));
 
-const button = new Clipboard("button");
-
-function handleCopy() {
-  console.log("Copiado");
+const button = new ClipboardJS("button");
+console.log(button)
+function handleCopy(event: ClipboardJS.Event) {
+  console.log(event.action, event.text);
+  event.clearSelection()
 }
 
 button.on("success", handleCopy);
