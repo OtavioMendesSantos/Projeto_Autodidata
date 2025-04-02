9d6f1b7bf2b40e1e77304b7b8ef94e2b1197c585
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-02 00:48:15 -0300

    :black_nib: course: 0606 Preencher Tabela

--- TypeScript/Origamid/06-Pratica_dados/src/main.ts
+++ TypeScript/Origamid/06-Pratica_dados/src/main.ts
@@ -7,7 +7,24 @@ async function handleData() {
   );
   if (!data) return;
   const transacoes = data.map((item) => normalizarTransacao(item));
+  preencherTabela(transacoes);
+}
+
+function preencherTabela(transacoes: Transacao[]): void {
   console.log(transacoes);
+  const tabela = document.querySelector<HTMLTableElement>("#transacoes tbody");
+  if (!tabela) return;
+  transacoes.forEach((transacao) => {
+    tabela.innerHTML += `
+    <tr>
+      <td>${transacao.nome}</td>
+      <td>${transacao.email}</td>
+      <td>R$ ${transacao.moeda}</td>
+      <td>${transacao.pagamento}</td>
+      <td>${transacao.status}</td>
+    </tr>
+    `;
+  });
 }
 
 handleData();
