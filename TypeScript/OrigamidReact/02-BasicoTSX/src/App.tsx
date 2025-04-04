c73f582c53b26b6d810f23e435336220fa44e303
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-04 03:14:38 -0300

    :black_nib: course: 0201 Inferência

--- TypeScript/OrigamidReact/02-BasicoTSX/src/App.tsx
+++ TypeScript/OrigamidReact/02-BasicoTSX/src/App.tsx
@@ -1,6 +1,24 @@
+import React from "react"
+
 const App = () => {
+  const [total, setTotal] = React.useState(0)
+
+  function increment() {
+    setTotal(total + 1)
+  }
+  function decrement() {
+    setTotal(total - 1)
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <p>{total}</p>
+      <div className="flex">
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
+      <button onClick={() => setTotal(0)}>Zerar</button>
+      </div>
+    </div>
   )
 }
 
