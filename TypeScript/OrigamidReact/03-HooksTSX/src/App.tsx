d011a872cc56ac43a7ba1e1cb46441801cd326ec
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-06 00:25:55 -0300

    chore: config pasta 3

---
+++ TypeScript/OrigamidReact/03-HooksTSX/src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import Button from "./components/Button"
+import Input from "./components/Input"
+import Checkbox from "./components/Checkbox"
+
+const App = () => {
+  const [total, setTotal] = React.useState(0)
+  const [nome, setNome] = React.useState("")
+
+  function increment() {
+    setTotal(total + 1)
+  }
+  function decrement() {
+    setTotal(total - 1)
+  }
+
+  return (
+    <div>
+      <p>{total}</p>
+      <div className="flex">
+        <Button onClick={() => setTotal(0)}>Zerar</Button>
+        <Button onClick={decrement}>Decrementar</Button>
+        <Button onClick={increment}>Incrementar</Button>
+      </div>
+      {nome}
+      <Input
+        type="text"
+        id="nome"
+        label="Nome"
+        placeholder="Digite seu nome"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <Checkbox label={'Termos e condições'}/>
+    </div>
+  )
+}
+
+export default App
