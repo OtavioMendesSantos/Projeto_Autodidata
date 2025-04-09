5c048a6e88babf2f81b3b62b54f0e1f18728ebac
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-09 01:36:32 -0300

    :black_nib: course: 0402 Páginas e Hooks

--- TypeScript/OrigamidReact/04-ProjetoTSX/src/App.tsx
+++ TypeScript/OrigamidReact/04-ProjetoTSX/src/App.tsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+import SideNav from "./components/SideNav";
+import Resumo from "./pages/Resumo";
 import "./Style.css";
 
 function App() {
-  return <div></div>;
+  return <main>
+    <Header />
+    <SideNav />
+    <Resumo />
+  </main>;
 }
 
 export default App;
---
+++ TypeScript/OrigamidReact/04-ProjetoTSX/src/hooks/useFetch.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+
+// Crie um custom hook chamado useFetch.
+// 1 - Este hook deve retornar a interface:
+interface FetchState<T> {
+    data: T | null;
+    loading: boolean;
+    error: string | null;
+}
+// Onde T é um valor genérico que deverá ser passado quando o Hook for utilizado.
+// 2 - data, loading e error são estados reativos (useState).
+// 3 - O hook deve receber a URL e OPTIONS como argumentos (interfaces de fetch).
+// 4 - O fetch deve ocorrer em um useEffect, com dependência apenas da URL.
+// 5 - Use AbortController para abortar o fetch caso o componente desmonte, antes do fetch ser concluído.
+// 6 - Teste o Hook com a api: https://data.origamid.dev/produtos
+
+function useFetch<T>(url: RequestInfo | URL, options?: RequestInit): FetchState<T> {
+    const [data, setData] = useState<T | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+
+    // É definido apenas uma vez e fica armazenado na memória;
+    const optionsRef = useRef(options);
+    optionsRef.current = options;
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const { signal } = controller;
+
+        async function handleRequest() {
+            try {
+                setLoading(true);
+                setError(null);
+                setData(null);
+
+                const response = await fetch(url, {
+                    signal,
+                    ...optionsRef.current
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Erro: ${response.status}`);
+                };
+
+                if (!signal.aborted) {
+                    const json = await response.json() as T;
+                    setData(json);
+                };
+            } catch (error: unknown) {
+                if (signal.aborted) return;
+
+                if (error instanceof Error) {
+                    setError(error.message)
+                } else {
+                    setError('Erro desconhecido')
+                }
+            } finally {
+                if (!signal.aborted) setLoading(false)
+            }
+        };
+
+        handleRequest();
+
+        return () => controller.abort();
+    }, [url])
+
+    return { data, loading, error };
+}
+
+export default useFetch
