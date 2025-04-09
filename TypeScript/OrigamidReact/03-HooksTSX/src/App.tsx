1f19aa11252d15b92ad4499bdb16dd51c315b8cd
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-08 23:43:04 -0300

    :black_nib: course: 0305 useFetch TSX 1

--- TypeScript/OrigamidReact/03-HooksTSX/src/App.tsx
+++ TypeScript/OrigamidReact/03-HooksTSX/src/App.tsx
@@ -1,12 +1,37 @@
 import React, { useRef } from "react"
 import Vendas from "./sections/Vendas"
 import Video from "./sections/Video";
+import useFetch from "./hook/useFetch";
+
+type Produto = {
+  id: string,
+  nome: string,
+  preco: number,
+  quantidade: number,
+  descricao: string,
+  internacional: boolean
+
+}
 
 const App = () => {
+  const produtos = useFetch<Produto[]>('https://data.origamid.dev/produtos')
+  const produto = useFetch<Produto>('https://data.origamid.dev/produtos/p001')
+
+
+  React.useEffect(() => {
+    console.log('data', produtos.data)
+    console.log('loading', produtos.loading)
+    console.log('error', produtos.error)
+  }, [produtos])
+
+  React.useEffect(() => {
+    console.log('produto', produto)
+  }, [produto])
+
   return (
     <div>
       {/* <Vendas /> */}
-      <Video />
+      {/* <Video /> */}
     </div>
   )
 }
---
+++ TypeScript/OrigamidReact/03-HooksTSX/src/hook/useFetch.tsx
@@ -0,0 +1,58 @@
+// Crie um custom hook chamado useFetch.
+
+import { useEffect, useState } from "react";
+
+// 1 - Este hook deve retornar a interface:
+interface FetchState<T> {
+    data: T | null;
+    loading: boolean;
+    error: string | null;
+}
+
+// Onde T é um valor genérico que deverá ser passado quando o Hook for utilizado.
+
+// 2 - data, loading e error são estados reativos (useState).
+
+// 3 - O hook deve receber a URL e OPTIONS como argumentos (interfaces de fetch).
+
+// 4 - O fetch deve ocorrer em um useEffect, com dependência apenas da URL.
+
+// 5 - Use AbortController para abortar o fetch caso o componente desmonte, antes do fetch ser concluído.
+
+// 6 - Teste o Hook com a api: https://data.origamid.dev/produtos
+
+function useFetch<T>(url: RequestInfo | URL, options?: RequestInit): FetchState<T> {
+    const [data, setData] = useState<T | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        async function handleRequest() {
+            try {
+                setLoading(true)
+                setError(null)
+                setData(null)
+
+                const response = await fetch(url, options);
+                const json: T = await response.json();
+
+                setData(json)
+            } catch (error: unknown) {
+                if (error instanceof Error) {
+                    setError(error.message)
+                } else {
+                    setError('Erro desconhecido')
+                }
+            } finally {
+                setLoading(false)
+            }
+
+        }
+
+        handleRequest();
+    }, [url])
+
+    return { data, loading, error };
+}
+
+export default useFetch
