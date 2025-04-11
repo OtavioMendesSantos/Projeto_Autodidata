5b54bdb6f78c764ba42b9087738ce0ef207352b2
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-11 19:09:01 -0300

    :black_nib: course: 0409 Resumo Vendas

--- TypeScript/OrigamidReact/04-ProjetoTSX/src/pages/Resumo.tsx
+++ TypeScript/OrigamidReact/04-ProjetoTSX/src/pages/Resumo.tsx
@@ -4,10 +4,43 @@ import { useData } from '../context/DataContext'
 const Resumo = () => {
   const { data } = useData();
 
-  // console.log(data)
+  if (data === null) return null;
 
   return (
-    <div>Resumo</div>
+    <section>
+      <div className='flex mb resumo'>
+        <div className='box'>
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status !== 'falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+        <div className='box'>
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === 'pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+        <div className='box'>
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === 'pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+      </div>
+      <div className="box">
+          graficos
+      </div>
+    </section>
   )
 }
 
