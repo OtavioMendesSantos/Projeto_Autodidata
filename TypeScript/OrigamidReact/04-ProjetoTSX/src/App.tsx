21b7e190de23e7f031ca060fb727ad4beeeb81aa
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-04-11 19:50:42 -0300

    :black_nib: course: 0412 Router TSX

--- TypeScript/OrigamidReact/04-ProjetoTSX/src/App.tsx
+++ TypeScript/OrigamidReact/04-ProjetoTSX/src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import { DataContextProvider } from "./context/DataContext";
 import Resumo from "./pages/Resumo";
 import Vendas from "./pages/Vendas";
+import Venda from "./pages/Venda";
 import "./Style.css";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <SideNav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <SideNav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
